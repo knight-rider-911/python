39eff17a30b37a6ab28438c98b983fc499535789
eagle <w@sd.ru>
2017-06-26 16:04:27 +0400

    fedora new chapter

--- Douson/testing/image.py
+++ Douson/testing/image.py
@@ -10,14 +10,14 @@ while red != 16:
             imgDrawer = ImageDraw.Draw(img)
             img.save("/home/eagle/img/red" + str(red) + "green" + str(green) + "blue" + str(blue) + ".png")
             print(red, green, blue)
-            blue=blue+1
+            blue+=1
         blue=0
-        green=green+1
+        green+=1
         print(red, green, blue)
     print(red, green, blue)
     green=0
-    blue-0
-    red=red+1
+    blue=0
+    red+=1
 
 print("end")
 
