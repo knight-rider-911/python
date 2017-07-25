c7b7518125561d51286dd709c23231abba713496
eagle <w@sd.ru>
2017-07-25 17:39:44 +0400

    fedora new chapter

--- Douson/testing/image.py
+++ Douson/testing/image.py
@@ -1,23 +1,12 @@
 from PIL import Image, ImageDraw
-blue=0
-red=0
-green=0
-while red != 16:
-    while green !=16:
-        while blue !=16:
-            color = (red*red, green*green, blue*blue)
-            img = Image.new('RGB', (100, 50), color)
-            imgDrawer = ImageDraw.Draw(img)
-            img.save("/home/eagle/img/red" + str(red) + "green" + str(green) + "blue" + str(blue) + ".png")
-            print(red, green, blue)
-            blue+=1
-        blue=0
-        green+=1
-        print(red, green, blue)
-    print(red, green, blue)
-    green=0
-    blue=0
-    red+=1
+blue=86
+red=1
+green=36
+color = (red, green, blue)
+img = Image.new('RGB', (100, 50), color)
+imgDrawer = ImageDraw.Draw(img)
+img.save("/home/eagle/img/red" + str(red) + "green" + str(green) + "blue" + str(blue) + ".png")
+print(red, green, blue)
 
 print("end")
 
