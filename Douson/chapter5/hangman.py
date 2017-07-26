c93a710a8ab81a2a730d2d3002a7fc011dcc712b
eagle <w@sd.ru>
2017-07-26 18:53:24 +0400

    hangman chapter 5

--- Douson/chapter5/hangman.py
+++ Douson/chapter5/hangman.py
@@ -5,4 +5,28 @@
 # отгадать за отведенное количество попыток. на экране появится фигурка повешенного.
 # ~мпорт модуля
 import random
-print()
+# константы
+HANGMAN = (
+    """
+    89
+    """
+)#нарисовать висельника
+MAX_WRONG = len(HANGMAN) - 1
+WORDS = ("OVERUSED", "CLAM", "GUAM", "TAFFETA", "PYTHON")
+# инициализация переменных
+word = random.choice(WORDS) # слово для отгадывания
+so_far = "-" * len(word) #по одному дефису на каждую букву. которую надо отгадать
+wrong = 0 # количество ошибок. которые сделал игрок
+used = [] # буквы. которые игрок уже предлагал
+print("Дoбpo пожаловать в игру 'Виселица'. Удачи вам!")
+while wrong < MAX_WRONG and so_far != word:
+    print(HANGMAN[wrong])
+    print("\nBы уже предлагали следующие буквы:\n", used)
+    print("\nОтгаданное вами в слове сейчас выглядит так:\n", so_far)
+guess = input("\n\nВведите букву: ")
+guess = guess.upper()
+while guess in used:
+    print("Bы уже предлагали букву", guess)
+    guess = input("\n\nBвeдитe букву: ")
+    guess = guess.upper()
+used.append(guess)
