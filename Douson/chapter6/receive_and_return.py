64e77c9734da3cd96c3b6df4d16aecbaf30e4a6c
eagle <w@sd.ru>
2017-07-27 18:27:32 +0400

    receive_and_return 6 chapter

---
+++ Douson/chapter6/receive_and_return.py
@@ -0,0 +1,20 @@
+# Принимай - возвращай
+# Демонстрирует параметры и возвращаемые значения
+def display(message) :
+    print(message)
+def give_me_five() :
+    five = 5
+    return five
+def ask_yes_no(question) :
+    """Задает вопрос с ответом 'да' или 'нет '. """
+    response = None
+    while response not in ("у", "n") :
+        response = input(question) .lower()
+    return response
+# основная часть
+display("Baм сообщение. \n")
+number = give_me_five()
+print("Boт что возвратила функция give_me_five() :", number)
+answer = ask_yes_no("\nПожалуйста. введите ·у· или 'n': ")
+print( "Спасибо. что ввели", answer)
+input("\n\nHaжмитe Enter. чтобы выйти.")
