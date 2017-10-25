a3fe6f19ab7ee04901c0f3e1f7907937b4de7cd0
michael <jmen.org@gmail.com>
2017-10-25 23:28:48 +0400

    scrap tictac

--- Douson/chapter6/tic-tac-toe.py
+++ Douson/chapter6/tic-tac-toe.py
@@ -2,9 +2,9 @@
 # Компьютер играет в крестики-нолики против пользователя
 # глобальные константы
 X = "Х"
-O = "O"
+O = "0"
 EMPTY = " "
-ITE = "Ничья"
+TIE = "Ничья"
 NUM_SQUARES = 9
 def displау_instruct():
     """Выводит на экран инструкцию для игрока."""
@@ -22,7 +22,7 @@ def displау_instruct():
 def ask_yes_no(question):
     """Задает вопрос с ответом 'Да' или 'Нет'."""
     response = None
-    while response not in ("у", "n"):
+    while response not in ("y", "n"):
         response = input(question).lower()
     return response
 def ask_number(question, low, high):
@@ -77,4 +77,88 @@ def winner(board):
                    (2, 5, 8),
                    (0, 4, 8),
                    (2, 4, 6))
-"""def"""
+    for row in WAYS_ТО_WIN:
+        if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
+            winner = board[row[0]]
+            return winner()
+        if EMPTY not in board:
+            return TIE
+    return None
+
+def human_move(board, human):
+    """Получает ход человека. """
+    legal = legal_moves(board)
+    move = None
+    while move not in legal:
+        move = ask_number("Tвoй ход. Выбери одно из полей (О - 8):", 0, NUM_SQUARES)
+        if move not in legal:
+            print("\nCмeшнoй человек! Это поле уже занято. Выбери дpyroe.\n")
+    print("Ладно...")
+    return move
+def computer_move(board, computer, human):
+    """Делает ход за компьютерного противника."""
+    #создадим рабочую копию доски. потому что функuия будет менять некоторые значения в списке
+    board = board[:]
+    # поля от лучшего к худшему
+    BEST_MOVES = (4, 0, 2, 6, 8, 1, 3, 5,7)
+    print("Я выберу поле номер", end=" ")
+
+    for move in legal_moves(board):
+        board[move] = computer
+        # если следующим ходом может победить компьютер. выберем этот ход
+        if winner(board) == computer:
+            print(move)
+            return move
+        # вь1полнив проверку. отменим внесенные изменения
+        board[move] = EMPTY
+    # поскольку следующим ходом ни одна сторона не может победить.
+    # выберем лучшее из доступных полей
+    for move in BEST_MOVES:
+        if move in legal_moves(board):
+            print(move)
+            return move
+def next_turn(turn):
+    """Осуществляет переход хода."""
+    if turn == X:
+        return O
+    else:
+        return X #Эта функция используется для того. чтобы чередовать ходы по мере того. как игроки будут их совершать.
+
+def congrat_winner(the_winner, computer, human):
+    """Поздравляе; победителя игры."""
+    if the_winner != TIE:
+        print("Tpи", the_winner, "в ряд!\n")
+    else:
+        print("Hичья!\n")
+    if the_winner == computer:
+        print("Kaк я и предсказывал. победа в очередной раз осталась за м~ой. \n" \
+              "Вот еще один довод в пользу того. что компьютеры превосходят людей решительно во всем.")
+    elif the_winner == human:
+        print("O нет. этого не может быть! Неужели ты как-то сумел перехитрить меня. белковый? \n" \
+              "Клянусь: я. компьютер. не допущу этого больше никогда!")
+    elif the_winner == TIE:
+        print("Teбe несказанно повезло. дружок: ты сумел свести игру вничью. \n" \
+              "Радуйся же сегодняшнему успеху! Завтра тебе уже не суждено его повторить. ")
+
+def main():
+    displау_instruct()
+    computer, human = pieces()
+    turn = X
+    board = new_board()
+    display_board(board)
+    while not winner(board):
+        if turn == human:
+            move = human_move(board, human)
+            board[move] = human
+        else:
+            move = computer_move(board, computer, human)
+            board[move] = computer
+        display_board(board)
+        turn = next_turn(turn)
+    the_winner = winner(board)
+    congrat_winner(the_winner, computer, human)
+
+
+# запуск программы
+main()
+input("\n\пНажмите Enter. чтобы выйти.")
--- scrap/scrapetest.py
+++ scrap/scrapetest.py
@@ -1,3 +1,4 @@
 from urllib.request import urlopen
 html = urlopen("http://pythonscraping.com/pages/page1.html")
+"""html = urlopen("http://bash.im/")"""
 print(html.read())
