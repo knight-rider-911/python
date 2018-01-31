296e5ae889b681bb47d51964b610fc6d919208c4
eagle <w@sd.ru>
2018-01-31 17:00:11 +0400

    getname python version2

---
+++ Douson/testing/rename-host.py
@@ -0,0 +1,12 @@
+import socket
+#print server name
+print(socket.gethostname())
+# change server name
+#socket.sethostname("fedora-nout")
+
+
+"""import platform
+platform.node()
+
+import socket
+socket.gethostname()"""
--- Douson/testing/t.py
+++
@@ -1,5 +0,0 @@
-import socket
-#print server name
-print(socket.gethostname())
-# change server name
-#socket.sethostname("fedora-nout")
