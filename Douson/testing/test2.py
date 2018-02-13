e657d266706eaf5237178ecf63b65eeddc1c2893
michael <jmen.org@gmail.com>
2018-02-13 20:57:36 +0400

    md5 checksumm

--- Douson/testing/test2.py
+++ Douson/testing/test2.py
@@ -1,6 +1,1 @@
-import os
-
-print(os.name)
-os.system('chcp  65001 ')
-os.system('cmd.exe')
-"""print(os.uname)"""
+import osprint(os.name)os.system('chcp  65001 ')os.system('cmd.exe')"""print(os.uname)"""
