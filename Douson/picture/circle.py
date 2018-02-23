e54d4bdee5d45bb7ac6dbd8bb20ddb6d9c48fa10
michael <jmen.org@gmail.com>
2018-02-23 21:58:26 +0400

    circle.py

--- Douson/picture/circle.py
+++ Douson/picture/circle.py
@@ -7,12 +7,12 @@ draw = ImageDraw.Draw(image)
 def to_radan(x):
     return x*pi/180.0
 
-count = 5
-while count <11:
+count = 25
+while count >5:
 
 
-    draw.ellipse((450-count*10, 450-count*10, 500+count*10, 500+count*10),  outline="red")
-    count+=1
+    draw.ellipse((450-count*10, 450-count*10, 500+count*10, 500+count*10), fill=(255-count*3,255-count*5,20+count*5), outline=(255-count*3,255-count*5,20+count*5))
+    count-=1
 
 
 
