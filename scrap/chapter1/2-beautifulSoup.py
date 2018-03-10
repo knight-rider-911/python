2372373919ed3386fe03eb6ba29ca07089d713fc
knight <knight@debian.com>
2018-03-10 18:32:42 +0400

    fractal

--- scrap/chapter1/2-beautifulSoup.py
+++ scrap/chapter1/2-beautifulSoup.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 #html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
-html = urlopen("http://ithappens.me/story/4")
+html = urlopen("http://ithappens.me/story/40")
 
 bsObj = BeautifulSoup(html, "html.parser")
 print(bsObj.h1)
