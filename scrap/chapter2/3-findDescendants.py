5c3d719b7478f8cd5ea7747f0b2209a8d36903f5
knight <jmen.org@gmail.com>
2019-05-05 08:42:26 +0300

    scrap and study

--- scrap/chapter2/3-findDescendants.py
+++ scrap/chapter2/3-findDescendants.py
@@ -1,10 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-# html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-html = urlopen("https://ithappens.me/story/9191")
-
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
 
-for child in bsObj.find("div",{"class":"text"}).children:
+for child in bsObj.find("table",{"id":"giftList"}).children:
     print(child)
