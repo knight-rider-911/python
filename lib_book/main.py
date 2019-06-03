42cc3e6efb927cbda955cc8561508a0a4da5352a
knight <jmen.org@gmail.com>
2019-06-04 00:14:50 +0300

    mauk

--- lib_book/main.py
+++ lib_book/main.py
@@ -13,17 +13,16 @@ def check_sum(path):
 
 
 
-# init_cat='d:/E-Book/E-book/Science'
-init_cat='/home/knight/Network/'
+init_cat='d:/E-Book/E-book/network'
+# init_cat='/home/knight/Network/'
 contain=os.walk(init_cat)
 file_export = open('text.csv', 'w')
 for (root, dirs, files) in os.walk(init_cat):
     for file in files:
         fullpath = os.path.join(root,file)
-
-        print(fullpath.replace('\\','/'))
+        fullpath = fullpath.replace('\\','/')
+        print(fullpath)
         print(file +'  ' + str(os.path.getsize(fullpath))+' checksum is'+check_sum(fullpath)+ '\n\n\n')
-
         file_export.write(fullpath+';'\
                           + str(os.path.getsize(fullpath))+';'
                           + str(check_sum(fullpath))+';'+'\n'
