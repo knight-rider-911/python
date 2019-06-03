609a25af413599f7d73148b7d642de32ad411cb2
knight <jmen.org@gmail.com>
2019-06-03 23:51:13 +0300

    main lib

--- lib_book/main.py
+++ lib_book/main.py
@@ -1,25 +1,32 @@
-import os
+import os,hashlib
 
-init_cat='d:/E-Book/E-book/Science'
+
+
+def check_sum(path):
+    sha256_hash = hashlib.sha256()
+    with open(path,"rb") as f:
+    # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+        str_check_sum=str(sha256_hash.hexdigest())
+    return str_check_sum
+
+
+
+# init_cat='d:/E-Book/E-book/Science'
+init_cat='/home/knight/Network/'
 contain=os.walk(init_cat)
-# for item in os.walk(init_cat):
-#     print(item)
-# # print(contain[1])
+file_export = open('text.csv', 'w')
+for (root, dirs, files) in os.walk(init_cat):
+    for file in files:
+        fullpath = os.path.join(root,file)
 
-# f = []
-# for (dirpath, dirnames, filenames) in os.walk(init_cat):
-#     f.extend(str(dirpath)+str(dirnames)+str(filenames))
-#
-# for item in f:
-#     print(item)
+        print(fullpath.replace('\\','/'))
+        print(file +'  ' + str(os.path.getsize(fullpath))+' checksum is'+check_sum(fullpath)+ '\n\n\n')
 
-# contain= os.listdir(init_cat)
-# for i in contain:
-#     print(i)
-for (root, dirs, files) in os.walk(contain):
-    print(files)
-    # for dir in dirs:
-    #     print(dir)
+        file_export.write(fullpath+';'\
+                          + str(os.path.getsize(fullpath))+';'
+                          + str(check_sum(fullpath))+';'+'\n'
+                          )
 
-    # for _file in files:
-    #     print(_file)
+file_export.close()
