0b5522b11aa817c89dc401f532b73b20d8417964
knight <jmen.org@gmail.com>
2020-01-25 22:55:43 +0300

    add new file found dublicate file

---
+++ lib_book/compare_lib.py
@@ -0,0 +1,63 @@
+import os,hashlib
+
+def check_sum(path):
+    sha256_hash = hashlib.sha256()
+    with open(path,"rb") as f:
+    # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+        str_check_sum=str(sha256_hash.hexdigest())
+    return str_check_sum
+
+
+init_cat='e:/tmp_book/'
+# compare_cat='d:/data/knight/YandexDisk/Book for read/'
+# init_cat='e:/data/Firefly/Фото/ЛИСА/100NIKON'
+# init_cat='/home/knight/Network/'
+contain=os.walk(init_cat)
+# file_export = open('text_d.csv', 'w')
+file_ar=['']
+comp_file_ar=['']
+
+for (root, dirs, files) in os.walk(init_cat):
+    for file in files:
+        fullpath = os.path.join(root,file)
+        fullpath = fullpath.replace('\\','/')
+        # print(fullpath)
+        # print(file +'  ' + str(os.path.getsize(fullpath))+' checksum is'+check_sum(fullpath)+ '\n\n\n')
+        file_ar.append(fullpath)
+        # file_export.write(fullpath+';'\
+        #                   + str(os.path.getsize(fullpath))+';'
+        #                   + str(check_sum(fullpath))+';'+'\n'
+        #                   )
+
+# for (root, dirs, files) in os.walk(compare_cat):
+#     for file in files:
+#         fullpath = os.path.join(root,file)
+#         fullpath = fullpath.replace('\\','/')
+#         comp_file_ar.append(fullpath)
+# i=0
+# it=0
+# while i < len(file_ar):
+#     it = 0
+#     while it<len(comp_file_ar):
+#         if str(os.path.basename(file_ar[i]))==str(os.path.basename(comp_file_ar[it])):
+#             print(str(file_ar[i])+'    '+str(comp_file_ar[it]))
+#         it=it+1
+#     # print(i)
+#     i=i+1
+i=0
+while i < len(file_ar):
+    # print(file_ar[i]+'\n\n\n\n\n')
+    it=0
+    while it < len(file_ar):
+        # print(file_ar[it]+'123132')
+        # if (str(check_sum(file_ar[i]))==str(check_sum(file_ar[it]))):
+        #     print("Found"+file_ar[i])
+        if str(os.path.basename(file_ar[i]))==str(os.path.basename(file_ar[it])) and (i!=it):
+            print('Found '+ str(file_ar[i])+ '    and     '+str(file_ar[it])  +' checksum  '+ str(check_sum(file_ar[i])) )
+        it=it+1
+    i=i+1
+
+
+
