819e36f360538811b434865a0edd9bc7128b6c10
knight <jmen.org@gmail.com>
2021-12-12 21:41:26 +0300

    some change

--- project/library/create_cache_file.py
+++ project/library/create_cache_file.py
@@ -10,21 +10,60 @@ def check_sum(path):
         str_check_sum=str(sha256_hash.hexdigest())
     return str_check_sum
 
-def create_dump_folder(init_cat):
-    f = open('dump.txt', 'w')
+#func to init file
+def init_create_dump_folder(init_cat):
+    src_dump_file = init_cat+'/dump.txt'
+    f = open(src_dump_file, 'w', encoding='utf-8')
     print(init_cat)
     for (root, dirs, files) in os.walk(init_cat):
         for file in files:
-            print(init_cat)
             fullpath = os.path.join(root, file)
-            print('Fullpath='+fullpath+'\n')
             fullpath = fullpath.replace('\\', '/')
             f.write(fullpath + ';' + check_sum(fullpath) + '\n')
-                # tmp_file.append(fullpath + ';' + check_sum(fullpath))
+          # tmp_file.append(fullpath + ';' + check_sum(fullpath))
     f.close()
 
-init_cat='/Users/mcheryasov/code/private/code-python/'
-create_dump_folder(init_cat)
+def check_status(init_cat):
+    src_dump_file = init_cat+'/dump.txt'
+    file_tree_files=['']
+    file_tree_src = ['']
+    try:
+        file_dump = open(src_dump_file, 'r', encoding='utf-8')
+    except FileNotFoundError:
+        print('Файл не существует!')
+    for file_line in file_dump:
+        file_tree_files.append(file_line)
+    print(init_cat)
+    for (root, dirs, files) in os.walk(init_cat):
+        for file in files:
+            fullpath = os.path.join(root, file)
+            fullpath = fullpath.replace('\\', '/')
+            file_tree_src.append(fullpath)
+    i=0
+    file_tree_src.pop(0)
+    print("Reading folder is done")
+    print(len(file_tree_files))
+    print('\n')
+    print(len(file_tree_src))
+    while i<len(file_tree_src):
+        j = 0
+        find_unidex_file = False
+        while j<len(file_tree_files):
+            file_orig = str(file_tree_files[j].split(';')[0])
+            if str(file_tree_src[i])!=file_orig:
+                find_unidex_file=True
+            j += 1
+        if find_unidex_file:
+            print(file_tree_src[i]+'           '+file_orig+'\n')
+        i+=1
+
+    file_dump.close()
+# init_cat='/Users/mcheryasov/code/private/code-python/'
+init_cat='d:/tmp_book/'
+#disable so it's init func
+# init_create_dump_folder(init_cat)
+
+check_status(init_cat)
 
 
 
@@ -54,4 +93,4 @@ create_dump_folder(init_cat)
 #                 print('File '+tmp_file[i].split(';')[0]+'  duplicate with  '+ tmp_file[j].split(';')[0]+'   '+tmp_file[i].split(';')[1] +'\n')
 #         j += 1
 #     i += 1
-# print("Job complete successfully")
+# # print("Job complete successfully")
